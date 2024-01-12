4839698513b1fe6734ac11634de7415a9dfed0ac
IshikaDhavan <107250389+IshikaDhavan@users.noreply.github.com>
2024-01-12 18:36:33 +0530

    playerMovement.js

--- Paintball-Arena/playerMovement.js
+++ Paintball-Arena/playerMovement.js
@@ -1,12 +1,12 @@
-AFRAME.registerComponent("PlayerMovement",{
+AFRAME.registerComponent("player-movement",{
     init:function(){
         this.walk();
     },
     walk:function(){
         window.addEventListener("keydown",(e) => {
-            if(e.key == "ArrowUp" || e.key == "ArrowRight" || e.key == "ArrowDown" || e.key == "ArrowLeft"){
+            if(e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "ArrowRight" || e.key == "ArrowLeft"){
                 var entity = document.querySelector("#sound2")
-                entity.components.sound.playSound()
+                entity.components.sound.playSound();
             }
         })
     }
